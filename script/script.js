b2d85da36b463b8f5f5ae3f836254ad54914a656
Konstantin Petrenko <109843396+KestPetrenko@users.noreply.github.com>
2022-09-09 09:22:16 +0300

    Add files via upload
    new

---
+++ script/script.js
@@ -0,0 +1,31 @@
+
+
+    let element;
+    let result;
+
+    function Clean() {
+        element = document.getElementById("imputWindow");
+        element.value = "";
+    }
+
+    function Take(symbol) {
+        element = document.getElementById("imputWindow");
+        element.value += symbol;
+    }
+
+    function Result() {
+        element = document.getElementById("imputWindow");
+        result = eval(element.value);
+        Clean();
+        element = document.getElementById("imputWindow");
+        element.value = result;
+    }
+
+    function Delete() {
+        element = document.getElementById("imputWindow");
+        let exp = element.value;
+        let end = exp.length - 1;
+        exp = element.value.slice(0, end);
+        element.value = exp;
+    }
+    
